93dc1cf7d2118d3a9c157d4f9408ae00caec8682
KrisD1977 <derela@student.wszib.edu.pl>
2021-05-06 21:34:35 +0200

    service

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {MainPageComponent} from './main-page/main-page.component';
+import {CapitalComponent} from './capital/capital.component';
 
 const routes: Routes = [
-  {path: '', component: MainPageComponent}
+  {path: '', component: MainPageComponent},
+  {path: 'capital', component: CapitalComponent}
 ];
 
 @NgModule({
