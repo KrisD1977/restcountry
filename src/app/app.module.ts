57e82500edaeabef66d81a9cff2ad417203c1dd4
KrisD1977 <derela@student.wszib.edu.pl>
2021-05-06 07:22:23 +0200

    mainpage

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainPageComponent } from './main-page/main-page.component';
+import { CapitalComponent } from './capital/capital.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainPageComponent,
+    CapitalComponent
   ],
   imports: [
     BrowserModule,
